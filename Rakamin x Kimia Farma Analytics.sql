56fe32911980be7c50ac734fe89762288d390f6b
Nurfadhilah2503 <102144300+Nurfadhilah2503@users.noreply.github.com>
2025-03-21 11:33:49 +0800

    Create Rakamin x Kimia Farma Analytics.sql

---
+++ Rakamin x Kimia Farma Analytics.sql
@@ -0,0 +1,35 @@
+SELECT
+    t.transaction_id,
+    t.date,
+    b.branch_id,
+    b.branch_name,
+    b.kota,
+    b.provinsi,
+    b.rating AS branch_rating,
+    t.customer_name,
+    p.product_id,
+    p.product_name,
+    p.price AS actual_price,
+    t.discount_percentage,
+    t.rating AS transaction_rating,
+    CASE
+        WHEN p.price <= 50000 THEN 0.10
+        WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
+        WHEN p.price > 100000 AND p.price <= 300000 THEN 0.20
+        WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
+        ELSE 0.30
+    END AS persentase_gross_laba,
+    (p.price - (p.price * t.discount_percentage)) AS nett_sales,
+
+    (p.price * 
+    (CASE
+        WHEN p.price <= 50000 THEN 0.10
+        WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
+        WHEN p.price > 100000 AND p.price <= 300000 THEN 0.20
+        WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
+        ELSE 0.30
+    END) 
+  )AS nett_profit
+FROM Kimia_Farma.KF_Transactions t
+LEFT JOIN Kimia_Farma.KF_Products p ON t.product_id = p.product_id
+LEFT JOIN Kimia_Farma.KF_Branches b ON t.branch_id = b.branch_id
